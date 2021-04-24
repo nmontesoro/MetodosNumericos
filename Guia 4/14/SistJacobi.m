1f257c670e57503ef6038d96a115f8a4e86b3a92
Nazareno Montesoro <n.montesoro@gmail.com>
2021-04-24 10:33:02 -0300

    Comprueba condiciones del input

--- Guia 4/14/SistJacobi.m
+++ Guia 4/14/SistJacobi.m
@@ -20,6 +20,21 @@ function [X, n] = SistJacobi(A, X, b, c)
 %   X: matriz columna con las soluciones del sistema
 %   n: número de iteraciones requeridas
     
+    size_A = size(A);
+    size_X = size(X);
+
+    if size_A(1) ~= size_A(2)
+        error('La matriz A no es cuadrada - el sistema no tiene solución')
+    end
+
+    if size_X(2) ~= 1
+        error('X no es un vector columna')
+    end
+
+    if size_X(1) ~= size_A(1)
+        error('El tamaño de X no coincide con el de A')
+    end
+    
     opt.B = CalcMatrizBJacobi(A);
     opt.c = CalcMatrizCJacobi(A, b);
     opt.X_a = X;
