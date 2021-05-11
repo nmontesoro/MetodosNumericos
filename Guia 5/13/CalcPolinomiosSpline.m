a18ea4ae5b92d8b2bc082d9825d15ba359b9f4ad
Nazareno Montesoro <n.montesoro@gmail.com>
2021-05-11 16:09:23 -0300

    Soporte para coeficientes personalizados

--- Guia 5/13/CalcPolinomiosSpline.m
+++ Guia 5/13/CalcPolinomiosSpline.m
@@ -1,8 +1,8 @@
-function pols = CalcPolinomiosSpline(points, sujeta, f_0, f_n)
+function pols = CalcPolinomiosSpline(points, varargin)
 %CalcPolinomiosSpline - Encuentra los polinomios del spline (trazador 
 %   cúbico) según los puntos de interpolación.
 %
-% Syntax: pols = CalcPolinomiosSpline(points, sujeta, f_0, f_n)
+% Syntax: pols = CalcPolinomiosSpline(points, sujeta, varargin)
 %
 % Input:
 %   points: puntos a interpolar. Formato: [x1, y1; x2, y2; ...; xn, yn]
@@ -10,9 +10,25 @@ function pols = CalcPolinomiosSpline(points, sujeta, f_0, f_n)
 %       frontera sujeta (default = false).
 %   f_0, f_n: necesarios si sujeta == true. Valor de la derivada primera
 %       de f en x_0 y x_n.
+%   coefs: opcional. Matriz de coeficientes personalizada.
 %
 % Output:
 %   pols: array de funciones simbólicas, cada una con el polinomio
+
+    parser = inputParser;
+    validPoints = @(points) size(points, 2) == 2 && size(points, 1) > 1;
+    addRequired(parser, 'points', validPoints);
+    addParameter(parser, 'sujeta', false);
+    addParameter(parser, 'f_0', NaN);
+    addParameter(parser, 'f_n', NaN);
+    addParameter(parser, 'coefs', NaN);
+    parse(parser, points, varargin{:});
+
+    points = parser.Results.points;
+    sujeta = parser.Results.sujeta;
+    f_0 = parser.Results.f_0;
+    f_n = parser.Results.f_n;
+    coefs = parser.Results.coefs;
 
     [n, m] = size(points);
 
@@ -20,17 +36,12 @@ function pols = CalcPolinomiosSpline(points, sujeta, f_0, f_n)
         points = CalcDistanciasSpline(points);
     end
 
-    if ~exist('sujeta', 'var')
-        sujeta = false;
+    if sujeta && any(isnan([f_0, f_n]))
+        error('Se requieren los valores de la derivada en x_0 y x_n.');
     end
 
-    if sujeta
-        if ~exist('f_0', 'var') || ~exist('f_n', 'var')
-            error('Se requieren los valores de la derivada en x_0 y x_n.');
-        end
-        coef = CoeficientesSpline(points, sujeta, f_0, f_n);
-    else
-        coef = CoeficientesSpline(points);
+    if isnan(coefs)
+        coefs = CoeficientesSpline(points, sujeta, f_0, f_n);
     end
 
     pols = sym('p%d', [n - 1, 1]);
@@ -39,10 +50,10 @@ function pols = CalcPolinomiosSpline(points, sujeta, f_0, f_n)
     for i = 1:n - 1
         xk = points(i, 1);
 
-        p(x) = coef(i, 1) ...
-             + coef(i, 2) * (x - xk) ...
-             + coef(i, 3) * (x - xk) ^ 2 ...
-             + coef(i, 4) * (x - xk) ^ 3;
+        p(x) = coefs(i, 1) ...
+             + coefs(i, 2) * (x - xk) ...
+             + coefs(i, 3) * (x - xk) ^ 2 ...
+             + coefs(i, 4) * (x - xk) ^ 3;
 
         pols(i) = p(x);
     end
