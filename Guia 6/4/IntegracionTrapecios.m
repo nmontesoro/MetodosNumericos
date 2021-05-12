bd55b6c093eb45ec611b321610027dd0483cf50c
Nazareno Montesoro <n.montesoro@gmail.com>
2021-05-12 11:30:14 -0300

    Commit inicial

---
+++ Guia 6/4/IntegracionTrapecios.m
@@ -0,0 +1,50 @@
+function val = IntegracionTrapecios(varargin)
+    parser = inputParser;
+    isValidN = @(n) ~isinf(n) && floor(n) == n && n >= 1;
+    addParameter(parser, 'a', NaN);
+    addParameter(parser, 'b', NaN);
+    addParameter(parser, 'points', NaN);
+    addParameter(parser, 'n', NaN);
+    addParameter(parser, 'f', NaN);
+    parse(parser, varargin{:});
+
+    a = parser.Results.a;
+    b = parser.Results.b;
+    n = parser.Results.n;
+    points = parser.Results.points;
+    f = parser.Results.f;
+
+    if ~isnan(points) & ~isnan(f)
+        warning('Se pasaron parámetros para f y points - se ignora este último')
+    end
+
+    if isnan(points)
+        if isnan(n)
+            n = 1;
+        end
+
+        if isnan(f) || isnan(a) || isnan(b)
+            error('Faltan parámetros')
+        else
+            X = linspace(a, b, n + 1);
+            Y = f(X);
+            points = [X.', Y.'];
+        end
+    else
+        if isnan(n)
+            n = size(points, 1) - 1;
+        end
+    end
+
+    N = size(points, 1);
+    if n >= N
+        error('n debe ser < que la cantidad de puntos')
+    end
+
+    h = (points(N, 1) - points(1, 1)) / (2 * n);
+    val = points(1, 2) + points(N, 2);
+    if n ~= 1
+        val = val + 2 * sum(points(2:N-1, 2));
+    end
+    val = h * val;
+end
