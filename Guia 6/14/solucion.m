dad22c5303a64e60b0d02845174233741b473f52
Nazareno Montesoro <n.montesoro@gmail.com>
2021-05-16 10:04:16 -0300

    Corrijo tolerancia y respuestas

--- Guia 6/14/solucion.m
+++ Guia 6/14/solucion.m
@@ -1,7 +1,7 @@
 % No sé si hay una forma de determinar los índices i y j para cierta 
 %   tolerancia, por eso lo hago a lo bruto.
-% a)  R8,1 = 0.4309641 (err: 0.0000003)
-% b) R13,1 = 0.6666659 (err: 0.0000008)
+% a)  R3,3 = 0.4309641 (err: 0.0000003)
+% b) R12,2 = 0.6666658 (err: 0.0000009)
 
 [val, err, i, j] = RombergBruto(0.5, 1);
 fprintf('a) R%d,%d = %8.7f (err: %8.7f)\n', i, j, val, err);
@@ -18,7 +18,7 @@ function [val, err, i, j] = RombergBruto(a, b)
         for j = 1:i
             val = IntegracionRomberg(f, i, j, a, b);
             err = err_abs(val, 2/3 * (b^(3/2) - a^(3/2)));
-            if err < 1e-3
+            if err < 1e-6
                 break;
             end
         end
