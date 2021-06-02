21a7798e0264965b74ab839805c224d8794f680a
Nazareno Montesoro <n.montesoro@gmail.com>
2021-06-02 10:36:53 -0300

    Integro por Euler para que coincida con la cátedra

--- Guia 7/12/solucion.m
+++ Guia 7/12/solucion.m
@@ -1,5 +1,6 @@
 f = @(X) X(3);
 g = @(X) X(1) + (1 - X(1) / 5) * X(2);
-X = ResolverPVF([1, 4], 1, [2, -1], 'disparo', 'f', {f, g})
+X = ResolverPVF([1, 4], 1, [2, -1], 'disparo', 'f', {f, g}, ...
+    'metodo_integracion', 'euler')
 
 % TODO: Documentar y agregar la solución del inciso (b)
